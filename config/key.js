14be7a2c15685aa74f87ba04e11a9e33c3c0e53d
mubashirrehman9 <55541376+mubashirrehman9@users.noreply.github.com>
2023-01-11 20:48:50 +0500

    file uploaded

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MongoURI: "mongodb+srv://honrhodev:honrhodev45@cluster0.kzgx07v.mongodb.net/authdb?retryWrites=true&w=majority"
+    // MongoURI: "mongodb://admin:admin987@cluster0-shard-00-00.r3fs6.mongodb.net:27017,cluster0-shard-00-01.r3fs6.mongodb.net:27017,cluster0-shard-00-02.r3fs6.mongodb.net:27017/auth?authSource=admin&replicaSet=atlas-638q0p-shard-0&readPreference=primary&ssl=true"
+}
---
+++ routes/index.js
@@ -0,0 +1,67 @@
+let fs = require('fs');
+let path = require('path');
+const express = require('express');
+var app = express();
+
+const router = express.Router();
+var multer = require('multer');
+const decompress = require("decompress");
+const fsExtea = require('fs-extra');
+const { ensureAuthenticated } = require('../config/checkAuth')
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        var dir = './uploads';
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+        callback(null, dir);
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+
+//------------ Welcome Route ------------//
+router.get('/', (req, res) => {
+    res.render('welcome');
+});
+
+//------------ Dashboard Route ------------//
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dash', {
+    name: req.user.name
+}));
+
+var upload = multer({ storage: storage }).single('uebuild');
+router.post('/upload', ensureAuthenticated, function (req, res, next) {
+    upload(req, res, async function (err) {
+        if (err) {
+            return res.end("Something went wrong:(");
+        }
+        const uploadDir = path.join(__dirname, '../', 'uploads')
+        const buildversionDir = path.join(__dirname, '../', 'buildversion')
+        const liveserverDir = path.join(__dirname, '../', 'liveserver')
+        const buildversionFolder = fs.readdirSync(buildversionDir);
+        const uploadedFiles = fs.readdirSync(uploadDir)
+        const mainUploadFile = path.join(uploadDir, uploadedFiles[0])
+
+
+        await fsExtea.copy(liveserverDir, path.join(buildversionDir, 'buildV' + (buildversionFolder.length + 1)))
+        fs.rmSync(liveserverDir, { recursive: true, force: true });
+        var dir = './liveserver';
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+        decompress(mainUploadFile, liveserverDir)
+            .then((files) => {
+                console.log(files);
+                fs.unlinkSync(mainUploadFile)
+
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        res.end("Upload completed.");
+    });
+})
+
+module.exports = router;
